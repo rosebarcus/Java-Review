6bd7e6fa17739fb0124d9530ede7f06ffdf35542
Rose Barcus <angela.rose.barcus@gmail.com>
2021-09-02 15:11:08 -0500

    Reviewing the basics of Java

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,5 +1,18 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 23;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I am reviewing Java and it's great!";
+        System.out.println(myString);
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
     }
 }
