b1fcaa7fe337c2631002e752151b9afe100155fd
Rose Barcus <angela.rose.barcus@gmail.com>
2021-09-10 14:30:46 -0500

    second push from set up of repo

---
+++ src/test/java/com/example/javareview/JavaReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javareview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
